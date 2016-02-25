18d3b9750a0e8f4dbda0f88d5dc15ffa9d9a16eb
Bryant-Curtis <cbryant0430@gmail.com>
2016-02-25 01:31:27 -0500

    Add paperclip and imagemagick APIs

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Paperclip.options[:command_path] = "/usr/local/bin/convert"
